69f90e8fb5961b5baa9744b525edd0a0914eb537
ashwinharidas515 <ashwinharidas515@gmail.com>
2020-03-10 06:19:24 +0530

    master

---
+++ palliandrome.js
@@ -0,0 +1,18 @@
+var x=3433;
+var y=x;
+var rev=0;
+var rem=0;
+while(x>0)
+{
+    rem=x%10
+    rev=rev*10+rem
+    x=parseInt(x/10)
+}
+if(rev==y){
+    console.log(rev)
+    console.log("it is a palliandrome")
+}
+else{
+    console.log("it is not a palliandrome")
+}
+
